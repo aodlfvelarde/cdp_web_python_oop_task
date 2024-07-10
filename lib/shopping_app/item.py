24b7abdbb3e8e28ddec737181b37bc8cd7ba9337
aodlfvelarde <129921638+aodlfvelarde@users.noreply.github.com>
2024-07-09 23:20:27 -0400

    test2

--- lib/shopping_app/item.py
+++ lib/shopping_app/item.py
@@ -1,11 +1,36 @@
-class Item:
+
+class Ownable:
+    def __init__(self, owner):
+        self._owner = owner
+
+    @property
+    def owner(self):
+        return self._owner
+
+    @owner.setter
+    def owner(self, new_owner):
+        self._owner = new_owner
+
+class Wallet:
+    def __init__(self, balance=0):
+        self.balance = balance
+
+class User:
+    def __init__(self, name):
+        self.name = name
+        self.wallet = Wallet()
+
+
+from ownable import Ownable
+
+class Item(Ownable):
     instances = []
 
     def __init__(self, name, price, owner=None):
+        Ownable.__init__(self, owner)
         self.name = name
         self.price = price
-        self.set_owner(owner)
-        # Itemインスタンスの生成時、そのItemインスタンス(self)は、insntancesというクラス変数に格納されます。
+        # Itemインスタンスの生成時、そのItemインスタンス(self)は、instancesというクラス変数に格納されます。
         Item.instances.append(self)
 
     def label(self):
@@ -15,3 +40,4 @@ class Item:
     def item_all():
         # instancesを返します ==> Item.item_all()でこれまでに生成されたItemインスタンスを全て返すということです。
         return Item.instances
+
--- lib/shopping_app/seller.py
+++ lib/shopping_app/seller.py
@@ -1,5 +1,17 @@
+class Ownable:
+    def __init__(self, owner):
+        self._owner = owner
+
+    @property
+    def owner(self):
+        return self._owner
+
+    @owner.setter
+    def owner(self, new_owner):
+        self._owner = new_owner
 from user import User
 
-class Seller(User):
+class Seller(User, Ownable):
     def __init__(self, name):
-        super().__init__(name)
+        User.__init__(self, name)  # Inicializar la parte de User
+        Ownable.__init__(self, self)
